e8c6a3471ba169126482efc5715bc5c5f414d36d
AleksanderAleksandrovich <aleksveles228@gmail.com>
2024-12-24 20:20:54 +0300

    add AppLink

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,32 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import classes from "./AppLink.module.scss";
+
+import { Link, LinkProps } from "react-router-dom";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  INVERTED = "inverted",
+}
+
+type AppLinkProps = {
+  children: React.ReactNode;
+  theme?: AppLinkTheme;
+} & LinkProps;
+
+export const AppLink = ({
+  className,
+  children,
+  theme = AppLinkTheme.PRIMARY,
+  to,
+  ...otherProps
+}: AppLinkProps) => {
+  return (
+    <Link
+      to={to}
+      className={classNames(classes.AppLink, {}, [className, classes[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
