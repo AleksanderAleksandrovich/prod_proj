3f3d7d5289464f19eae94a5e30782319966536a4
AleksanderAleksandrovich <aleksveles228@gmail.com>
2024-12-25 23:28:39 +0300

    delete ThemeSwitcher from Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,8 +4,6 @@ import { classNames } from "shared/lib/classNames/classNames";
 
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
-
 type NavbarProps = {
   className?: string;
 };
@@ -13,7 +11,6 @@ type NavbarProps = {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(classes.Navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={classNames(classes.links)}>
         <AppLink
           to={"/"}
