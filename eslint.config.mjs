a5fab576e975c8e196418c122d1e02022a390d75
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-01 18:07:04 +0300

    structuring eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,31 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import stylisticJSX from "@stylistic/eslint-plugin-jsx";
+import i18next from "eslint-plugin-i18next";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  },
+  // { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+
   { ignores: ["node_modules", "build"] },
+
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+
+  { settings: { react: { version: "detect" } } },
+
+  js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  i18next.configs["flat/recommended"],
+
   stylisticJSX.configs["all-flat"],
   {
     rules: {
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+
       "@stylistic/jsx/jsx-indent": ["warn", 2],
       "@stylistic/jsx/jsx-indent-props": ["warn", 2],
       "@stylistic/jsx/jsx-one-expression-per-line": "off",
