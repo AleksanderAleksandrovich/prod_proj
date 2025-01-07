fb4387f82250ca1f0fcdb41756b2a16615937e4a
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-07 21:05:11 +0300

    refactor plugins for prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,7 +18,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: Options): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -32,8 +32,12 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: isDev,
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+  return plugins;
 }
